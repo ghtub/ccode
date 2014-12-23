1084f7561a1f86ec04e3b228163ca1352729c102
ghtub <vik.reddy@gmail.com>
2014-12-22 18:48:03 -0800

    added I/O stuff

--- TODOlist.c
+++ TODOlist.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include "myHeader.h"
 
 int main()
 {
   printf("Hi! Back from your morning run?");
-  printf("Time to...build, write, read, play guitar!!");
-
+  printf(" Time to...build, write, read, play guitar!!");
+    
+   printf("What do you want to do today? \n");
+   
+   struct Item one;
+   one.id = 1; /* id of ITEM is 1 */
+   fgets(one.text,100,stdin); /* reads user input into text var, size = 100, std input */
+   printf("Great! So the first thing to do today is... %s \n",one.text);
   return 0;
 }
